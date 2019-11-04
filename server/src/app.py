e2d888e64386d1d14154bff0ad0d947512964507
村上 哲太郎 <murakami_tetsutaro@abema.tv>
2019-11-04 17:26:03 +0900

    fix

--- server/src/app.py
+++ server/src/app.py
@@ -13,7 +13,12 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 df = pd.read_csv('./server/src/assets/13TOKYO.csv')
 
 print('df', df)
-
+# print('dataframeの行数・列数の確認==>\n', df.shape)
+# print('indexの確認==>\n', df.index)
+# print('columnの確認==>\n', df.columns)
+# print('dataframeの各列のデータ型を確認==>\n', df.dtypes)
+#print('drop', df.drop(df.index[[1, 3, 5]]))
+#print('col', df.drop(df.columns[[1,2,3]]))
 def generate_table(dataframe, max_rows=10):
     return html.Table(
         # Header
@@ -37,14 +42,15 @@ app.layout = html.Div(children=[
     html.Label('Dropdown'),
     dcc.Dropdown(
         options=[
-            {'label': '失敗', 'value': 'FAIL'},
-            {'label': u'成功', 'value': 'SUCCES'},
+            {'label': u'千代田区', 'value': 'CHIYODA'},
+            {'label': '新宿区', 'value': 'SHINJUKU'},
         ],
-        value='SUCCES'
+        value='CHIYODA'
     ),
 
     html.H4(children='test result'),
-    generate_table(df, 48),
+    #generate_table(df.sort_values(by='旧郵便番号', axis=0), 200),
+    generate_table(df.query('市区町村名 == "新宿区"'), 200),
 
     dcc.Graph(
         id='example-graph',
